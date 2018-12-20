c939735afa4d8764f3482f2cedf4b9072871c618
ESITES\rvoermans <rvoermans@e-sites.nl>
2018-12-20 13:12:39 +0100

    microservice games

---
+++ games/src/main/java/com/rickvoermans/microservices/games/GamesApplication.java
@@ -0,0 +1,14 @@
+package com.rickvoermans.microservices.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesApplication.class, args);
+    }
+
+}
+
---
+++ games/src/test/java/com/rickvoermans/microservices/games/GamesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rickvoermans.microservices.games;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
